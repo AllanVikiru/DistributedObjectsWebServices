ad95e56a4e434aba4d9b01bc3c1a4f0724d6650f
Allan Vikiru <vikiruallan12@gmail.com>
2020-11-20 13:55:47 +0300

    update - students.sql format

--- students.sql
+++ students.sql
@@ -1,66 +1,18 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
+CREATE TABLE `students` ( 
+  `admNo` INT(10) NOT NULL AUTO_INCREMENT ,
+  `firstName` VARCHAR(255) NOT NULL , 
+  `lastName` VARCHAR(255) NOT NULL , 
+  `dateofBirth` DATE NOT NULL , 
+  `school` VARCHAR(8) NOT NULL , 
+  `course` VARCHAR(8) NOT NULL , 
+  PRIMARY KEY (`admNo`)
+) ENGINE = InnoDB;
 
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `students`
---
-
-CREATE TABLE `students` (
-  `admNo` int(10) NOT NULL,
-  `firstName` varchar(255) NOT NULL,
-  `lastName` varchar(255) NOT NULL,
-  `dateofBirth` date NOT NULL,
-  `school` varchar(8) NOT NULL,
-  `course` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `students`
---
-
-INSERT INTO `students` (`admNo`, `firstName`, `lastName`, `dateofBirth`, `school`, `course`) VALUES
-(1, 'David', 'Kamau', '1997-04-10', 'SCES', 'BICS'),
-(2, 'Mary', 'Otieno', '1998-03-29', 'SCES', 'BBIT'),
-(3, 'Wendy', 'Kirui', '1997-11-11', 'SCES', 'BTC'),
-(4, 'Moustafa', 'Ali', '1998-07-02', 'SCES', 'BICS'),
-(5, 'Fatuma', 'Khadija', '1997-12-13', 'SCES', 'BICS'),
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `students`
---
-ALTER TABLE `students`
-  ADD PRIMARY KEY (`admNo`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `students`
---
-ALTER TABLE `students`
-  MODIFY `admNo` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `students` 
+  (`admNo`, `firstName`, `lastName`, `dateofBirth`, `school`, `course`) 
+  VALUES
+  (1, 'David', 'Kamau', '1997-04-10', 'SCES', 'BICS'),
+  (2, 'Mary', 'Otieno', '1998-03-29', 'SCES', 'BBIT'),
+  (3, 'Wendy', 'Kirui', '1997-11-11', 'SCES', 'BTC'),
+  (4, 'Moustafa', 'Ali', '1998-07-02', 'SCES', 'BICS'),
+  (5, 'Fatuma', 'Khadija', '1997-12-13', 'SCES', 'BICS');
